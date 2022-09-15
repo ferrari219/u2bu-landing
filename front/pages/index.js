9c8c18871f9ce3dbb159eb592daf2cace1418cb0
ferrari219@nate.com <ferrari219@nate.com>
2022-09-15 23:42:26 +0900

    Head 추가

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Heads from 'components/01_u2bu/common/Heads';
 
 const index = () => {
-  return <div>Hello world</div>;
+  return (
+    <>
+      <Heads title="어디어디" />
+      <div>Hello world</div>
+    </>
+  );
 };
 
 export default index;
