68cb883de487e2b5ff91dabcb7c762262dc0b265
ferrari219@nate.com <ferrari219@nate.com>
2022-11-17 19:04:18 +0900

    simul: front 설치, back signup 제작중

--- simul/back/routes/post.js
+++ simul/back/routes/post.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { Post, Image } = require('../models');
 
-//Signup
+//AddPost
 router.post('/', async (req, res, next) => {
   try {
     const post = await Post.create(
--- simul/back/routes/user.js
+++ simul/back/routes/user.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const { User } = require('../models');
 
 // SignUp
-router.post('/', (req, res, next) => {
-  res.send('/user');
+router.post('/', async (req, res, next) => {
+  try {
+    const user = await User.create({
+      userid: req.body.userid,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    return res.status(201).json(user);
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
 });
 
 module.exports = router;
