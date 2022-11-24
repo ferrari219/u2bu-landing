4822d5cfa43f1682ab35eb368eca700789f10e0c
ferrari219@nate.com <ferrari219@nate.com>
2022-11-24 18:07:33 +0900

    simul/front: signup 제작중

--- simul/front/pages/signup.js
+++ simul/front/pages/signup.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { useForm, Controller } from 'react-hook-form';
-import { Input } from 'antd';
+import { Form, Input } from 'antd';
 
 const signup = () => {
-  const { handleSubmit, control } = useForm();
+  const { handleSubmit, control, register } = useForm({
+    defaultValues: {
+      userid: 'admin',
+      password: '1',
+      passwordCheck: '1',
+    },
+  });
   return (
-    <div>
+    <Form>
       <dl>
         <dt>
           <label htmlFor="userid">아이디</label>
@@ -14,13 +20,33 @@ const signup = () => {
           <Controller
             name="userid"
             control={control}
-            render={({ field }) => {
-              <Input {...field} />;
-            }}
+            render={({ field }) => (
+              <Input {...register('userid')} {...field} readOnly />
+            )}
+          />
+        </dd>
+        <dt>
+          <label htmlFor="password">비밀번호</label>
+        </dt>
+        <dd>
+          <Controller
+            name="password"
+            control={control}
+            render={({ field }) => <Input type="password" {...field} />}
+          />
+        </dd>
+        <dt>
+          <label htmlFor="passwordCheck">비밀번호 한번더 입력</label>
+        </dt>
+        <dd>
+          <Controller
+            name="passwordCheck"
+            control={control}
+            render={({ field }) => <Input type="password" {...field} />}
           />
         </dd>
       </dl>
-    </div>
+    </Form>
   );
 };
 
