8aefdee827de1604e8542040ede715d54794555c
ferrari219@nate.com <ferrari219@nate.com>
2022-11-28 12:01:55 +0900

    simul/front 초기화 재셋팅

---
+++ simul/front/pages/_app.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import 'antd/dist/reset.css';
+
+const common = ({ Component }) => {
+  return (
+    <>
+      <Head>
+        <title>U2buLanding</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="description" content="U2buLanding" />
+      </Head>
+      <Component />
+    </>
+  );
+};
+common.proptypes = {
+  Component: PropTypes.elementType.isRequired,
+};
+
+export default common;
