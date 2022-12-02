e4ff590665c0aba2414e8cb741c73372d33048b5
ferrari219@nate.com <ferrari219@nate.com>
2022-12-02 17:32:38 +0900

    simul/front: pages/index 제작중

--- simul/front/pages/index.js
+++ simul/front/pages/index.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import { Button, Form, Input } from 'antd';
+import { Controller, useForm } from 'react-hook-form';
 
 const index = () => {
-  return <div>index</div>;
+  const { control, handleSubmit, register } = useForm({
+    defaultValues: {
+      applyName: '김철수',
+    },
+  });
+  return (
+    <Form onFinish={handleSubmit(() => console.log('test'))}>
+      <dl>
+        <dt>
+          <label htmlFor="applyName">아이디</label>
+        </dt>
+        <dd>
+          <Controller
+            name="applyName"
+            control={control}
+            render={({ field }) => (
+              <Input
+                {...register('applyName', {
+                  required: 'admin을 입력해주세요.',
+                })}
+                {...field}
+              />
+            )}
+          />
+        </dd>
+      </dl>
+      <div></div>
+      <div>
+        <Button type="primary" htmlType="submit">
+          응모하기
+        </Button>
+      </div>
+    </Form>
+  );
 };
 
 export default index;
