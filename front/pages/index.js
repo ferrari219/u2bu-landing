8f35c3102917b8d993af9dbf8b09f50677b3655e
ferrari219@nate.com <ferrari219@nate.com>
2022-12-14 23:47:24 +0900

    front: css수정

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import PostForm from 'components/01_u2bu/common/PostForm';
+import Layout from 'components/01_u2bu/common/Layout';
 
 const index = () => {
-  return (
-    <>
-      <h1>ApplyForm</h1>
-      <div>Hello world</div>
-      <section>
-        <PostForm />
-      </section>
-    </>
-  );
+	return (
+		<Layout paddingOn>
+			<h1>ApplyForm</h1>
+			<div>Hello world</div>
+			<section>
+				<PostForm />
+			</section>
+		</Layout>
+	);
 };
 
 export default index;
